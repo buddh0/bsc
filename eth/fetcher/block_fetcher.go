f0ce6c48b0093581c341f7a3c42d7d01e10814d0
buddh0 <galaxystroller@gmail.com>
2024-12-09 11:31:47 +0800

    fix eth

--- eth/fetcher/block_fetcher.go
+++ eth/fetcher/block_fetcher.go
@@ -497,6 +497,9 @@ func (f *BlockFetcher) loop() {
 			for peer, hashes := range request {
 				log.Trace("Fetching scheduled headers", "peer", peer, "list", hashes)
 
+				// Create a closure of the fetch and schedule in on a new thread
+				fetchHeader := f.fetching[hashes[0]].fetchHeader
+
 				go func(peer string) {
 					if f.fetchingHook != nil {
 						f.fetchingHook(hashes)
@@ -506,7 +509,7 @@ func (f *BlockFetcher) loop() {
 						go func(hash common.Hash) {
 							resCh := make(chan *eth.Response)
 
-							req, err := f.fetching[hashes[0]].fetchHeader(hash, resCh)
+							req, err := fetchHeader(hash, resCh)
 							if err != nil {
 								return // Legacy code, yolo
 							}
